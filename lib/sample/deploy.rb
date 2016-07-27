068292c056919d0f40169a24218c30e3f38948fa
Simonas Šerlinskas <simonas.serlinskas@nfq.com>
2016-07-27 13:21:09 +0300

    simplify templates

--- lib/sample/deploy.rb
+++ lib/sample/deploy.rb
@@ -1,38 +1,25 @@
-# config valid only for current version of Capistrano
-# lock '3.3.x'
-
 set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :deploy_to, '/var/www/my_app_name'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :scm, :rsync
+set :archive_cache, true
 
-# Default value for :scm is :git
-set :scm, :archive
+#Uncomment if you want to use AWS auto discover feature
+#set :aws_region, ''
+#set :aws_id, ''
+#set :aws_secret, ''
 
-# Default value for :format is :pretty
-# set :format, :pretty
+set :exclude, [
+  '.idea',
+  '.git'
+  'node_modules'
+]
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
+set :log_level, :info
 set :linked_files, fetch(:linked_files, []).push('app/config/parameters.yml')
-
-# Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('app/logs')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
